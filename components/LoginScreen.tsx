21440240e86d6c5db1998e88b1e319f2a4d71d33
Tyler Brown <brown.tyler.w@gmail.com>
2025-04-19 21:35:02 -0700

    Added ui-kitten. Created simple login page

---
+++ components/LoginScreen.tsx
@@ -0,0 +1,80 @@
+import { StyleSheet, View, TouchableWithoutFeedback } from "react-native";
+import { useState } from "react";
+import { Text, Input, Icon, IconElement } from "@ui-kitten/components";
+
+export default function LoginScreen() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [secureEntry, setSecureEntry] = useState(true);
+
+    const toggleSecurity = () => {
+        setSecureEntry(!secureEntry);
+    }
+
+    const AlertIcon = (props) => (
+        <Icon
+            {...props}
+            name='alert-circle-outline'
+        />
+    );
+
+    const renderCaption = () => {
+        return (
+            <View style={styles.captionContainer}>
+                {AlertIcon(styles.captionIcon)}
+                <Text style={styles.captionText}>
+                    Should contain at least 8 symbols
+                </Text>
+            </View>
+        )
+    }
+
+    const hidePassword = (props) => {
+        <TouchableWithoutFeedback onPress={toggleSecurity}>
+            <Icon
+                {...props}
+                name={secureEntry ? 'eye-off' : 'eye'}
+            />
+        </TouchableWithoutFeedback>
+    };
+
+    return (
+        <View>
+            <Text category='h1'>
+                Welcome to Sketch Bank!
+            </Text>
+            <Input
+                placeholder="Enter Username"
+                value={username}
+                onChangeText={newVal => setUsername(newVal)}
+            />
+            <Input
+                placeholder="Enter Password"
+                value={password}
+                caption={renderCaption}
+                accessoryRight={hidePassword}
+                secureTextEntry={secureEntry}
+                onChangeText={newVal => setPassword(newVal)}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    captionContainer: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    captionIcon: {
+      width: 10,
+      height: 10,
+      marginRight: 5,
+    },
+    captionText: {
+      fontSize: 12,
+      fontWeight: '400',
+      fontFamily: 'opensans-regular',
+      color: '#8F9BB3',
+    },
+  });
